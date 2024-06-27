073493416080bafc5c36734edcba6fbb22574680
Mirim <Mirim@DESKTOP-241ENV5>
2024-06-27 11:55:19 +0900

    #1 기본 자료형

--- CSPratice1/Program.cs
+++ CSPratice1/Program.cs
@@ -23,8 +23,51 @@ namespace CSPratice1
 
             /*** XML 주석 ***/
 
+            // #1 출력
             Console.WriteLine("Hello World");
             Console.Write("줄바꿈 싫어");
+
+            // #1 기본 자료형
+            Console.WriteLine(52 + 4 - 1);
+            //사칙연산자 = 산술 연산 = Artimetic Operator
+            // + - * / %
+
+            // 실수 (Real Number)
+            Console.WriteLine(52.0);
+
+            //문자
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+
+            //문자열(string)
+            Console.WriteLine("Hello World");
+            //Escape Character
+            Console.WriteLine("미\n\\림\t마\"고");
+
+            //문자열 연결 연산자(Concatenation Operator)
+            Console.WriteLine("Hello" + " " + "World");
+            
+            // 문자열 인덱스 접근 [ ]
+            Console.WriteLine("Hello World"[4]);    // "0" 출력
+            //Console.WriteLine("Hello World"[100]);  // 예외 발생
+            //Console.WriteLine("Hello World"[-3]);   // 예외 발생
+
+            // 주의
+            Console.WriteLine('가' + '힣');
+            // 문자열 연결 연산자X, 덧셈 연산자임
+
+            // 논리(Boolean)
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            // 비교 연산자 (Comparison operator)
+            // == != > < >= <=
+
+            //논리
+            // && || !   and or not
+            Console.WriteLine(!true);
+            Console.WriteLine(true && false);
+            Console.WriteLine(true || false);
         }
     }
 }
